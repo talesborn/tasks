90c468132d498ad5268bf88db2afa94d5d865b6b
Tales Born <prog18@ixcsoft.com.br>
2022-02-21 17:58:36 -0300

    feat: criado projeto tasks-backend

---
+++ tasks-backend/migrations/20220221202656_create_table_tasks.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('tasks', table => {
+      table.increments('id').primary();
+      table.string('desc').notNull();
+      table.datetime('estimateAt');
+      table.datetime('doneAt');
+      table.integer('userId').references('id').inTable('users').notNull();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('tasks');
+};
